2f958757c861eb624ec4d474444f5bca45d576b7
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2025-02-26 12:02:28 +0100

    test

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const iframe = useTemplateRef('iframe')
+const adjustHeight = () => {
+  console.log('iframe', iframe.value)
+  console.log('contentWindow', iframe.value.contentWindow)
+  if(iframe.value) {
+    iframe.value.style.height = (iframe.value.contentWindow.document.body.scrollHeight + 256) + 'px';
+  }
+}
+</script>
+
+<template>
+  <div>
+    <iframe ref="iframe" src="https://messsage.rsmsp.dk/iframe" @load="adjustHeight" frameborder="0" height="100%", width="100%" />
+  </div>
+</template>
+
+<style scoped></style>
