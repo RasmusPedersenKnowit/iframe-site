df97ca5ca8a7bb47eeeecb4d30280c2cf4e71b29
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2025-02-26 09:39:51 +0100

    all

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+onMounted(() => {
+  window.addEventListener("load", () => {
+    window.parent.postMessage(
+        { scrollHeight: document.body.scrollHeight + 'px' },
+        "*"
+    );
+  });
+
+  window.addEventListener("resize", () => {
+    window.parent.postMessage(
+        { scrollHeight: document.body.scrollHeight + 'px' },
+        "*"
+    );
+  });
+})
+</script>
+
+<template>
+  <div style="height: 800px; border: 1px orange solid; padding: 1rem">
+    <h1>Page: index</h1>
+    <NuxtLink to="/second">Second page</NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/second.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+onMounted(() => {
+  window.addEventListener("load", () => {
+    window.parent.postMessage(
+        { scrollHeight: document.body.scrollHeight },
+        "*"
+    );
+  });
+
+  window.addEventListener("resize", () => {
+    window.parent.postMessage(
+        { scrollHeight: document.body.scrollHeight },
+        "*"
+    );
+  });
+})
+</script>
+
+<template>
+  <div style="height: 600px; border: 1px orange solid; padding: 1rem">
+    <h1>Page: SECOND</h1>
+    <NuxtLink to="/">Second page</NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
