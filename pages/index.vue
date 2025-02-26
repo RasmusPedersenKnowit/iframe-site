245b4f288a36fc286b96ed57a25b0118f54a4f78
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2025-02-26 12:56:42 +0100

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@ const adjustHeight = () => {
 
 <template>
   <div>
-    <iframe ref="iframe" src="https://iframe.rsmsp.dk/iframe" @load="adjustHeight" frameborder="0" height="100%", width="100%" />
+    <iframe ref="iframe" src="https://iframe.rsmsp.dk/iframe" @load="adjustHeight" frameborder="0" height="100%" width="100%" />
   </div>
 </template>
 
