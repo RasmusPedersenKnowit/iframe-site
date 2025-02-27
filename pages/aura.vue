47cf8741e726992ba615383ecf0e4c5908e064de
Rasmus Pedersen <rasmus.pedersen@knowit.dk>
2025-02-27 08:40:43 +0100

    wip

---
+++ pages/aura.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const iframe = useTemplateRef('iframe')
+
+onMounted(() => {
+  window.addEventListener('message', (event) => {
+    console.log('🤖message', event.data)
+    if(!iframe.value) return
+    iframe.value.style.height = (event.data.windowHeight + 32) + 'px'
+  })
+})
+</script>
+
+<template>
+  <div>
+    <iframe ref="iframe" src="https://staging.aura.dk/internet-new/waao-internet-umbraco/" frameborder="0" height="100%" width="100%" style="overflow: clip;" />
+  </div>
+</template>
+
+<style scoped></style>
